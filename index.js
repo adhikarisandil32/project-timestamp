4b29ca62ad6bcd1ed2b9ba17fb42057d17fbe3c6
Sandil Adhikari <adhikarisandil32@gmail.com>
2024-04-22 20:35:19 +0545

    completed-timestamp-microservice

--- index.js
+++ index.js
@@ -2,31 +2,40 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors")
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+  res.sendFile(__dirname + "/views/index.html")
+})
 
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  const differentDateTypes = {}
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+  if (isNaN(Number(req.params.date))) {
+    differentDateTypes["unix"] = Date.parse(req.params.date)
+    differentDateTypes["utc"] = new Date(req.params.date).toUTCString()
+  } else {
+    differentDateTypes["unix"] = req.params.date
+    differentDateTypes["utc"] = new Date(
+      Date.parse(req.params.date)
+    ).toUTCString()
+  }
 
-
+  res.json(differentDateTypes)
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log("Your app is listening on port " + listener.address().port)
+})
